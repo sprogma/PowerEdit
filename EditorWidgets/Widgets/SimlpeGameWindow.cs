057df6d9e04ba5884e73eb10abc3a4ed3b9adf9a
sprogma <mux-228@yandex.ru>
2026-04-29 02:55:32 +0300

    add base class

---
+++ EditorWidgets/Widgets/SimlpeGameWindow.cs
@@ -0,0 +1,18 @@
+﻿using EditorFramework.ApplicationApi;
+using EditorFramework.Layout;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EditorFramework.Widgets
+{
+    public class SimlpeGameWindow : BaseWindow
+    {
+        byte[,] field;
+
+        public SimlpeGameWindow(IApplication app, ILayoutManager layout, byte[,] field) : base(app, layout)
+        {
+
+        }
+    }
+}
